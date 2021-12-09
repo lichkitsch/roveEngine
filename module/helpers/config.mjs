7ec6a084916271231d4cc076368c4099616247be
Zephyr <lichkitsch@gmail.com>
2021-12-08 23:00:00 -0600

    initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,127 @@
+export const ROVEENGINE = {};
+
+// Define constants here, such as:
+ROVEENGINE.foobar = {
+  'bas': 'ROVEENGINE.bas',
+  'bar': 'ROVEENGINE.bar'
+};
+
+ROVEENGINE.items = {
+  "types": {
+    "item": "rove.items.item",
+    "tool": "rove.items.tool",
+    "wpn": "rove.items.wpn",
+    "arm": "rove.items.arm",
+    "abi": "rove.items.abi"
+  },
+  "ranks": {
+    "f": {
+      "bonus": 1,
+      "die": "d4",
+      "cost": "4d4"
+    },
+    "d": {
+      "bonus": 2,
+      "die": "d6",
+      "cost": "6d6"
+    },
+    "c": {
+      "bonus": 3,
+      "die": "d8",
+      "cost": "8d8"
+    },
+    "b": {
+      "bonus": 4,
+      "die": "d10",
+      "cost": "10d10"
+    },
+    "a": {
+      "bonus": 5,
+      "die": "d12",
+      "cost": "12d12"
+    },
+    "s": {
+      "bonus": 6,
+      "die": "d20",
+      "cost": "20d20"
+    }
+  }
+}
+
+ROVEENGINE.traits = {
+  "str": {
+    "label": "Strength",
+    "score": 0
+  },
+  "dex": {
+    "label": "Dexterity",
+    "score": 0
+  },
+  "con": {
+    "label": "Constitution",
+    "score": 0
+  },
+  "int": {
+    "label": "Intelligence",
+    "score": 0
+  },
+  "wis": {
+    "label": "Wisdom",
+    "score": 0
+  },
+  "cha": {
+    "label": "Charisma",
+    "score": 0
+  }
+}
+
+ROVEENGINE.levels = {
+  "1st": {
+    "label": "1st level",
+    "dmg": "d4"
+  },
+  "2nd": {
+    "label": "2nd level",
+    "dmg": "d6"
+  },
+  "3rd": {
+    "label": "3rd level",
+    "dmg": "d8"
+  },
+  "4th": {
+    "label": "4th level",
+    "dmg": "d10"
+  },
+  "5th": {
+    "label": "5th level",
+    "dmg": "2d6"
+  },
+  "6th": {
+    "label": "6th level",
+    "dmg": "2d8"
+  },
+  "7th": {
+    "label": "7th level",
+    "dmg": "2d10"
+  },
+  "8th": {
+    "label": "8th level",
+    "dmg": "2d12"
+  },
+  "9th": {
+    "label": "9th level",
+    "dmg": "3d10"
+  },
+  "10th": {
+    "label": "10th level",
+    "dmg": "3d12"
+  },
+  "11th": {
+    "label": "11th level",
+    "dmg": "4d10"
+  },
+  "12th": {
+    "label": "12th level",
+    "dmg": "4d12"
+  }
+}
